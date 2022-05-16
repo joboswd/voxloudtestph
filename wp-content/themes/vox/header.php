7ba25b618a0d7ae322ef19b5fc3ab424eacc6c44
Jayson Obos <jayson.obos@lkg.com.au>
2022-05-17 05:42:27 +0800

    VOX-3: replaced vw with w

--- wp-content/themes/vox/header.php
+++ wp-content/themes/vox/header.php
@@ -32,21 +32,62 @@
 		<div class="wrapper">
 
 			<!-- header -->
-			<header class="header clear" role="banner">
+			<header class="header clear w-100" role="banner">
+				<div class="container">
+					<div class="d-flex align-items-center w-100">
+						<div class="mr-auto">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+								<img src="<?php echo get_template_directory_uri(); ?>/img/logo-bookmark.svg" class="bookmark-logo" alt="Bookmark">
+							</a>
+						</div>
 
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
+						<div class="ml-auto text-right">
+							<div class="header-menu__desktop d-lg-block d-none">
+								<div>
+									<?php wp_nav_menu(array('theme_location' => 'footer-menu', 'fallback_cb' => 'false')); ?>
+								</div>
+								<div>
+								<a href="#" class="btn">LOGIN</a>
+								</div>
+							</div>
+
+							<div class="d-lg-none">
+								<div id="nav-lines">
+									<span></span>
+									<span></span>
+									<span></span>
+									<span></span>
+								</div>
+							</div>
+						</div>
 					</div>
-					<!-- /logo -->
-
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
+				</div>
 			</header>
 			<!-- /header -->
+			<div class="burger-content vh-100 w-100 text-center">
+				<?php wp_nav_menu(array('theme_location' => 'header-menu', 'fallback_cb' => 'false')); ?>
+				<div class="social-menu flex justify-content-center w-100">
+					<ul>
+						<li>
+							<a href="<?php the_field('facebook', $postid); ?>" target="_blank">
+								<i class="fa fa-facebook" aria-hidden="true"></i>
+							</a>
+						</li>
+						<li>
+							<a href="<?php the_field('twitter', $postid); ?>" target="_blank">
+								<i class="fa fa-twitter" aria-hidden="true"></i>
+							</a>
+						</li>
+						<li>
+							<a href="<?php the_field('linkedin', $postid); ?>" target="_blank">
+								<i class="fa fa-linkedin" aria-hidden="true"></i>
+							</a>
+						</li>
+						<li>
+							<a href="<?php the_field('instagram', $postid); ?>" target="_blank">
+								<i class="fa fa-instagram" aria-hidden="true"></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
--- wp-content/themes/vox/js/scripts.js
+++ wp-content/themes/vox/js/scripts.js
@@ -9,10 +9,20 @@
 		$('#nav-lines').click(function () {
 			$(this).toggleClass('open');
 			$('body').toggleClass('overflow-hidden').toggleClass('w-100');
-			$('.header-nav').toggleClass('burger-opened');
-			$('main').toggleClass('with-filter');
+			$('.header').toggleClass('burger-opened');
 			$('.burger-content').toggleClass('burger-opened');
 		});
+
+		// to remove drawer menu classes upon 992
+		window.addEventListener('resize', () => {
+			if (window.innerWidth > 991) {
+				$('#nav-lines').removeClass('open');
+				$('body').removeClass('overflow-hidden').removeClass('position-fixed').removeClass('w-100');
+				$('.header').removeClass('burger-opened');
+				$('.burger-content').removeClass('burger-opened');
+			}
+		});
+
 		
 	});
 
